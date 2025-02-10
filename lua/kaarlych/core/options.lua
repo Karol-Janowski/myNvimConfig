554d5ef39ac2952fd353318e19a4053a44e4b148
karoljanowski <karoljanowski@yahoo.com>
2025-02-10 10:50:45 +0100

    added backspace, clipboard and splitwindow options

--- lua/kaarlych/core/options.lua
+++ lua/kaarlych/core/options.lua
@@ -25,7 +25,15 @@ opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
 opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start options
 
+-- clipboard 
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+
+-- split windows 
+opt.splitright = true -- split vertical window to the right
+opt.splitbelow = true -- split horizontal window to the bottom
 
 
 
