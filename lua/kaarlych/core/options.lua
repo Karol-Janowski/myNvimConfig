966a55d30c86059c4deae4c3ab4f0fb22ed24212
karoljanowski <karoljanowski@yahoo.com>
2025-02-10 10:36:03 +0100

    added some indent , tab and wrap options

--- lua/kaarlych/core/options.lua
+++ lua/kaarlych/core/options.lua
@@ -4,3 +4,11 @@ local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
+
+-- tabs & indentation 
+opt.tabstop = 2 -- spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces 
+opt.autoindent = true -- copt indent from current line when starting new one
+
+opt.wrap = false
